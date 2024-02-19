8ec2a5d8b35b2412a1d3c65e60cd305b01a02d24
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-19 19:12:37 +0500

    Node.js Q25

---
+++ Q25.js
@@ -0,0 +1,13 @@
+"use strict";
+// 25. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+Object.defineProperty(exports, "__esModule", { value: true });
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+let aliancolor = "green";
+aliancolor = "red";
+if (aliancolor === "green") {
+    console.log("Player just earned 5 points!");
+}
+else {
+    console.log("Player just earned 10 points!");
+}
